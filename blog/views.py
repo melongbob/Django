520e5ca54b6811c42d0f059ac8d73590b6ec5008
Yun <melongbob@gmail.com>
2019-09-13 19:39:45 -0700

    move posts to database

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Yun Kim',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 12, 2019'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 13, 2019'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
